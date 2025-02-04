daf530d02092b1640fe53451860965740f64a51e
Gabriel Viana <gaa.vianaa@gmail.com>
2025-02-04 11:38:05 -0300

    atualizações

--- 04-objetos-genericos.php
+++ 04-objetos-genericos.php
@@ -6,55 +6,52 @@
     <title>PHP - Objetos</title>
 </head>
 <body>
-    <h1>Trabalhando com objetos genericos</h1>
+    <h1>Trabalhando com objetos genéricos</h1>
     <hr>
-
-    <?php
-    // criando uma instancia de uma classe generica chamada stdClass (Stander Class)
-    $usuario = new stdClass();
-
-    $usuario->nome = "gaa";
-    $usuario->idade = 19;
-    $usuario->email = "gaa@gmail.com";
-    $usuario->telefone = ["11-96659-5738", "25629292"];
-
-    echo $usuario->nome;
-
-    echo "<p><mark>{$usuario->nome}</mark></p>";
-    ?>
-
-
-    <h2>Anlisando a estrutura de objetos</h2>
+<?php
+/* Criando uma instância de uma classe genérica chamada stdClass (Stander Class) */
+$usuario = new stdClass(); // instância ou objeto
+ 
+// Adicionando propriedades/atributos ao objeto
+$usuario->nome = "Loud Tinowns";
+$usuario->idade = 25;
+$usuario->email = "loudtinowns$gmail.com";
+$usuario->telefones = ["11-4002-8922", "11-99397-7998"];
+ 
+echo $usuario->nome;
+ 
+// Pode ser usado {} ao redor do objeto/propriedade na interpolação
+echo "<p><mark>{$usuario->nome}</mark></p>"; // interpolação
+?>
+ 
+    <h2>Analisando a estrutura do objeto</h2>
     <pre><?=var_dump($usuario)?></pre>
     <hr>
-
-    <h2>Saida de dados</h2>
+ 
+    <h2>Saída de dados</h2>
     <p>Nome: <?=$usuario->nome?></p>
-    <p>idade: <?=$ususario->idade?></p>
-    <p>celular: <?=$usuario->telefone[1]?></p>
-
+    <p>Idade: <?=$usuario->idade?> anos</p>
+    <p>Celular: <?=$usuario->telefones[1]?></p>
+ 
     <hr>
-
+ 
     <h2>Convertendo (fazendo um casting) objeto em array associativo</h2>
-
-    <?php
-    $arrayUsuario = (array) $usario;
-    ?>
-
+ 
+<?php $arrayUsuario = (array) $usuario; ?>
+ 
     <h3>Analisando o array gerado a partir de um objeto</h3>
     <pre><?=var_dump($arrayUsuario)?></pre>
-
-    <h2>convertendo (fazendo um casting) array associativo em objetos</h2>
-
-    <?php
-    $aluno = ["nome" => "aaaa", "sobrenome" => "bbbbbbbb"];
-
-    $objAluno = (object) $aluno;
-    ?>
-
+ 
+    <h2>Convertendo (fazendo um casting) array associativo em objeto</h2>
+ 
+<?php
+// Array associativo com duas chaves (nome e sobrenome)
+$aluno = ["nome" => "Loud", "sobrenome" => "Tinowns"];
+ 
+// Convertendo array para objeto
+$objAluno = (object) $aluno;
+?>
     <h3>Analisando o objeto gerado a partir de um array</h3>
     <pre><?=var_dump($objAluno)?></pre>
-
-
 </body>
 </html>
---
+++ 05-condicionias.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - condicionais</title>
+</head>
+<body>
+    <h1>Estrutura condicionais</h1>
+    <hr>
+
+    <h2>Simples</h2>
+    <h3><code>if</code></h3>
+
+    <?php
+    $numero = 5;
+    
+    if($numero > 1){
+        echo "<p>$numero é maior que 1</p>";
+    }
+    ?>
+    
+</body>
+</html>
