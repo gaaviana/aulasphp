69b5d1c90157df936070c3b9d6d95c3ae2231336
Gabriel Viana <gaa.vianaa@gmail.com>
2025-02-06 11:49:39 -0300

    exercicio 04 (array associativo)

---
+++ exercicios/exercicio04.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 04</title>
+    <style>
+        html {
+            font-family: Georgia, 'Times New Roman', Times, serif;
+        }
+
+        table {
+            margin: auto;
+            border-collapse: collapse;
+            width: 50%;
+            text-align: center;
+        }
+
+        th,td {
+            border: solid 1px;
+            padding: 10px;
+        }
+
+        /* par */
+        tr:nth-child(even) {
+            background-color: lightgray; 
+        }
+    </style>
+</head>
+<body>
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>LINGUAGEM</th>
+            <th>DESCERIÇÃO</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+$dados = [
+    "HTML" => "Estruturação",
+    "CSS" => "Estilos",
+    "JS" => "Comportamentos",
+    "PHP" => "Back-End",
+    "SQL" => "Manipulação de dados",
+    "Java" => "Softwares"
+];
+
+$i = 1;
+foreach ($dados as $linguagem => $descricao) {  
+?>
+    <tr>
+        <td><?=$i?></td>
+        <td><?=$linguagem?></td>
+        <td><?=$descricao?></td>
+    </tr>
+<?php
+$i++;
+};
+?>
+    </tbody>
+</table>
+    
+</body>
+</html>
