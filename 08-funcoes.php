91d233c2ee8bedb6ae8272393954b1db0f70c546
Gabriel Viana <gaa.vianaa@gmail.com>
2025-02-11 09:17:14 -0300

    função com parametro opcional

--- 08-funcoes.php
+++ 08-funcoes.php
@@ -79,6 +79,16 @@
             <p>Reprovado.....</p>
         <?php } ?> 
         
+        <h3>Função com parâmetros opcionais</h3>
+
+        <?php 
+        function exibirSaudacao($mensagem, $pessoa = ""){
+            return "Olá, $mensagem $pessoa";
+        }
+        ?>
+
+        <p>$mensagem 1: <?=exibirSaudacao("bom dia", "gabriel")?></p>
+        <p>$mensagem 1: <?=exibirSaudacao("bom dia")?></p>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
