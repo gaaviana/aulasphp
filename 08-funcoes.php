e0eb2efc3eb4b983b322e9d6c2a33f2f8b5a79d4
Gabriel Viana <gaa.vianaa@gmail.com>
2025-02-11 09:49:34 -0300

    refatora ifelse

--- 08-funcoes.php
+++ 08-funcoes.php
@@ -97,12 +97,14 @@
         <?php
         //Indicando que o parâmetro DEVE ser do tipo inteiro, e que o retorno da função DEVE  ser do tipo string
         function verificarNegativo(int $valor):string{
-            if ($valor < 0) {
+            /* if ($valor < 0) {
                 return "É negativo";
             } else {
                 return "Não é negativo";
-            }
-        }
+            } */
+
+            return $valor < 0 ? "É negativo" : "Não é negativo";
+       }
 
         /* Tipos comuns par uso de indução 
         string = texto
