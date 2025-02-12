e2f70b441cf3549b90c40f7e31ae0103e5c813db
Gabriel Viana <gaa.vianaa@gmail.com>
2025-02-12 09:13:12 -0300

    fim exercicio

--- exercicios/exercicio05.php
+++ exercicios/exercicio05.php
@@ -9,40 +9,58 @@
 </head>
 <body>
     <div class="container">
-        <?php
-            $calculo = fn(float $nota1, $nota2, $nota3):float => ($nota1 + $nota2 + $nota3) / 3; 
-        ?>
-         
-        <?php
-            $media = $calculo(7,0,0);
 
-            function situacao($media){
-                if ($media < 7) return "reprovado";
-                 
-                return "aprovado";
-            }
-
-
-            $alunos = [
-                "A" => $calculo(10,10,10),
-                "B" => $calculo(0,10,0),
-                "C" => $calculo(7,10,8),
-                "D" => $calculo(10,5,4),
-                "E" => $calculo(5,1,7)
-            ] 
-        ?>
-
-
-        <p><?=$alunos["A"]?></p>
-        <p><?=$alunos["B"]?></p>
-        <p><?=$alunos["C"]?></p>
-        <p><?=$alunos["D"]?></p>
-        <p><?=$alunos["E"]?></p>
-
+        <h1 class="text-center">EXERCICIO - 05</h1>
         <hr>
-        <p><?=situacao($media)?></p>
-        <p><?=$media?></p>
-       
+
+<?php
+    $calculo = fn(float $nota1, $nota2, $nota3):float => ($nota1 + $nota2 + $nota3) / 3; 
+
+    $media = $calculo(7,0,0);
+  
+
+    function situacao($media){
+        if ($media < 7) return "reprovado";
+            
+        return "aprovado";
+    }
+
+
+    $alunos = [
+        "gaa" => $calculo(4,3,0),
+        "fasdf" => $calculo(0,10,0),
+        "adfdsaf" => $calculo(7,10,8),
+        "fsdfd" => $calculo(10,5,4),
+        "Afsdfxcv" => $calculo(5,1,7)
+    ];
+
+?>
+        <table class="table table-bordered border-dark text-center table-hover">
+            <thead>
+               <tr>
+                    <th class="table-dark">Nome</th>
+                    <th class="table-dark">Media</th>
+                    <th class="table-dark">Situação</th>
+               </tr> 
+            </thead>
+            <tbody>
+            <?php
+foreach ($alunos as $aluno => $media) {
+    $mediaF = number_format($media, 1);
+ 
+    $bgClass = ($media > 7) ? 'bg-primary-subtle' : 'bg-danger-subtle';
+?>
+    <tr>    
+        <td><?=$aluno?></td>
+        <td><?=$mediaF?></td>
+        <td class="<?= $bgClass ?>"><?=situacao($media)?></td>
+    </tr>
+<?php
+}
+?>    
+            </tbody>
+        </table>   
+        <hr>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
