04e5982e3449dc5b0fd03c899e3447d1c9f3cce0
Gabriel Viana <gaa.vianaa@gmail.com>
2025-02-13 11:55:23 -0300

    exportação de arquivps para php

--- 10.include-e-required.php
+++ 10.include-e-required.php
@@ -1,3 +1,4 @@
+<?php require "recursos.php"?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -11,6 +12,24 @@
     <div class="container">
         <h1>Modularização e Inclusão de Recursos</h1>
         <hr>
+        <h2> <?=ESCOLA?> </h2>
+        <p>Estamos fazendo o curso de <?=$curso?> </p>
+        <ul>
+            <?php foreach ($tecnologias as $tecnologia) { ?>
+                <li><?=$tecnologia?></li>
+            <?php }; ?>
+        </ul>
+
+        <hr>
+
+        <p>O aluno Fulano de Tal tem 20 anos e é <?=verificarIdade(20)?> de idade</p>
+
+        <article>
+            <h2>Texto qualquer...</h2>
+                <!-- incluindo fragmentos de html -->
+                <?php include "textos.php";?>
+
+        </article>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
---
+++ textos.php
@@ -0,0 +1,8 @@
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae neque inventore consequatur. Error assumenda illum dignissimos delectus, sequi dolore. Adipisci!</p>
+
+<?php include "lista.html"; ?>
+
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae neque inventore consequatur. Error assumenda illum dignissimos delectus, sequi dolore. Adipisci!</p>
+
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae neque inventore consequatur. Error assumenda illum dignissimos delectus, sequi dolore. Adipisci!</p>
+
